5bc3cf737ed88a67e5dc455d843e4c765557f041
OlaniyiHope <olaniyihoppee@gmail.com>
2023-02-19 15:13:58 +0100

    added tech and tool stack

--- src/components/header/data.js
+++ src/components/header/data.js
@@ -1,25 +1,25 @@
 import { FaAccessibleIcon, FaReact, FaGg } from "react-icons/fa";
 import { SiDatabricks, SiIonic } from "react-icons/si";
-import { AiOutlineGithub} from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
 import { MdConnectWithoutContact } from "react-icons/md";
 
 export const FeatureList = [
   {
     id: 1,
-    icon: <FaReact  size={22} className="text-color bold" />,
-    heading: "Front-end development",
-    text: "For frontend development, I'm a huge fan of React.js and it's ecosystem (e.g. Next.js & Gatsby.js). I also use bootstrap and good in craco, mdb-ui-kit, react-bootstrap etc",
+    icon: <FaReact size={22} className="text-color bold" />,
+    heading: "For web development",
+    text: "For web development, I'm a huge fan of React.js and node js, react and it's ecosystem (e.g. Next.js & Gatsby.js). I also use bootstrap and good in craco, mdb-ui-kit, react-bootstrap etc",
   },
   {
     id: 2,
-    icon: <SiIonic size={22} className="text-color bold"/>,
+    icon: <SiIonic size={22} className="text-color bold" />,
     heading: "Mobile App development",
     text: "For mobile app development, I use react native(Bare native and Expo cli), I also work with ionic cordova(React)",
   },
   {
     id: 3,
-    icon: <AiOutlineGithub  size={22} className="text-color bold"/>,
+    icon: <AiOutlineGithub size={22} className="text-color bold" />,
     heading: "Version control",
     text: "For deployment, i use the popular version control tool(Github). I also make use of heroku and netlify for other activities",
-  }
+  },
 ];
